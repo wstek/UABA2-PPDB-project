55b006b7e0c0d5e845d1b1cf349fa05152ecf7ae
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2022-04-27 22:44:39 +0200

    import boostrap js for nav collapse button

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.min.js";
 
 const rootElement = document.getElementById("root");
 
