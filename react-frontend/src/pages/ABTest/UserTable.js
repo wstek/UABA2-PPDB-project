cb9476d15cbe22aadcf90aab09973030a4aafe74
William-Steklein <st.william24@gmail.com>
2022-05-09 18:56:40 +0200

    refactor & usertable

--- react-frontend/src/pages/ABTest/UserTable.js
+++ react-frontend/src/pages/ABTest/UserTable.js
@@ -1,59 +1,88 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import {DataGrid} from '@mui/x-data-grid';
-import {useState} from "react";
 
-function DataTable({abtest_id}){
-const [loaded,setLoaded] = useState(false);
-const [allUsers,setAllUsers] = useState(null);
-const [allRows,setAllRows] = useState(null);
+function DataTable({abtest_id}) {
+    const [loaded, setLoaded] = useState(false);
+    // const [allUsers, setAllUsers] = useState(null);
+    const [allRows, setAllRows] = useState(null);
 
-const columns = [
-    {field: 'id', headerName: 'ID', width: 70}
-];
+    const columns = [
+        {field: 'id', headerName: 'ID', width: 70}
+    ];
 
-const getRows = () => {
-    let a = '/api/users/' + abtest_id
-    fetch('/api/users/' + abtest_id, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {"Content-Type": "application/json", 'Accept': 'application/json'}
-    }).then(res => res.json())
-        .then((data) => {
-            if (data.error) {
-                throw Error(data.error);
-            }
-            setLoaded(true)
-            setAllUsers(data.userlist)
-            console.log(data.userlist)
+    useEffect(() => {
+        fetch('/api/users/' + abtest_id, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {"Content-Type": "application/json", 'Accept': 'application/json'}
+        }).then(res => res.json())
+            .then((data) => {
+                if (data.error) {
+                    throw Error(data.error);
+                }
+                setLoaded(true)
+                // setAllUsers(data.userlist)
 
-        }).catch((err) => {
-        console.log(err);
-    })
-}
-function rows(){
-    let list = []
-    let users = allUsers
-    if(users != null) {
-        for (let i = 0; i < users.length; i++) {
-            list.push({id: users[i]})
-        }
-        setAllRows(list)
-        return allRows
-    }
-    return []
-}
+                let row_list = []
+                for (let i = 0; i < data.userlist.length; i++) {
+                    row_list.push({id: data.userlist[i]})
+                }
+
+                setAllRows(row_list)
+
+                // console.log(data.userlist)
+
+            }).catch((err) => {
+            console.log(err);
+        })
+    }, [abtest_id]);
+
+    // const getRows = () => {
+    //     let a = '/api/users/' + abtest_id
+    //     fetch('/api/users/' + abtest_id, {
+    //         method: 'GET',
+    //         credentials: 'include',
+    //         headers: {"Content-Type": "application/json", 'Accept': 'application/json'}
+    //     }).then(res => res.json())
+    //         .then((data) => {
+    //             if (data.error) {
+    //                 throw Error(data.error);
+    //             }
+    //             setLoaded(true)
+    //             setAllUsers(data.userlist)
+    //             console.log(data.userlist)
+    //
+    //         }).catch((err) => {
+    //         console.log(err);
+    //     })
+    // }
+
+    // function rows() {
+    //     let list = []
+    //     let users = allUsers
+    //     if (users != null) {
+    //         for (let i = 0; i < users.length; i++) {
+    //             list.push({id: users[i]})
+    //         }
+    //         setAllRows(list)
+    //         return allRows
+    //     }
+    //     return []
+    // }
 
     return (
         <div style={{height: 400, width: '100%'}}>
-            {!loaded && getRows()}
-            <DataGrid
-                rows={rows()}
+            {/*{!loaded && getRows()}*/}
+            {loaded && <DataGrid
+                rows={allRows}
                 columns={columns}
                 pageSize={5}
                 rowsPerPageOptions={[5]}
                 checkboxSelection
-            />
+            />}
         </div>
     );
 }
+
 export default DataTable
