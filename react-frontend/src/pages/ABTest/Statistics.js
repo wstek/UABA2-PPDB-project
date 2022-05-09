1e702910ba9327f450fa237d5f6f928f1118c70e
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2022-05-09 19:02:24 +0200

    added date slider

--- react-frontend/src/pages/ABTest/Statistics.js
+++ react-frontend/src/pages/ABTest/Statistics.js
@@ -1,11 +1,13 @@
 import Overview from "../../components/Overview";
 import LineChart from "../../components/LineChart";
 import {ColoredLine} from '../../components/ColoredLine';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import ABTestPicker from "../../components/ABTestpicker";
 import {fetchData} from "../../utils/fetchAndExecuteWithData";
+import DateSlider from "../../components/DateSlider";
 
 import SearchUser from "./SearchUser";
+
 function Statistics() {
     // Currently selected ab test id
     const [selected_abtest, setSelectedABTest] = useState(null)
@@ -23,21 +25,20 @@ function Statistics() {
     const [attributionRate, setAttributionRate] = useState(null);
     // The Purchases of the abtest
     const [revenuePerUser, setRevenuePerUser] = useState(null);
-    // const [fetches, setFetches] = useState([]);
+    const [ABTest_data, setABTestData] = useState(null);
+    const [StartIndex, setStartIndex] = useState(null);
+    const [EndIndex, setEndIndex] = useState(null);
 
-    //todo garbage remove
-    // popularity retrain look back
-    // recency retrain
-    // itemknn k,window, normalize retrain
-    const algoritmdict = [{Algorithm: "recency", retrain: 10, name: "algorithmLin"},
-        {Algorithm: "popularity", retrain: 3, window: 30, name: "algorithmidExp"},
-        // {Algorithm: "itemknn",
-        // retrain: 40, window: 9, K: 70, Normalize: 1, name: "algorithm3"}
-    ]
 
     function fetchCurrentUserABTestIDs() {
         let url = '/api/abtest/statistics/'
         fetchData(url, setPersonalABTests)
+    }
+
+    function fetchABTestData(abortCont) {
+        setABTestData(null)
+        let url = '/api/abtest/statistics/' + selected_abtest + '/ABTest_information'
+        fetchData(url, setABTestData, abortCont)
     }
 
     function fetchInputParameters(abortCont) {
@@ -57,21 +58,23 @@ function Statistics() {
         setPurchases(null)
         fetchData('/api/abtest/statistics/' + selected_abtest + '/purchases_over_time', setPurchases, abortCont)
     }
+
     function fetchCTROverTime(abortCont) {
         setClickThroughRate(null)
         fetchData('/api/abtest/statistics/' + selected_abtest + '/CTR_over_time', setClickThroughRate, abortCont)
     }
 
     useEffect(fetchCurrentUserABTestIDs, [],);
-
     useEffect(() => {
         const abortCont = new AbortController();
 
         if (selected_abtest) {
+            fetchABTestData(abortCont)
             fetchInputParameters(abortCont);
             fetchInputActiveUsersOverTime(abortCont);
             fetchInputPurchasesOverTime(abortCont)
             fetchCTROverTime(abortCont)
+
         }
 
         return () => abortCont.abort();
@@ -98,12 +101,15 @@ function Statistics() {
         setSelectedABTest(null)
     }
 
+    // console.log(ABTest_data && ABTest_data.dates)
     return (
         <div className="container-fluid  p-0 my-auto">
             <div className="row text-center align-items-center mb-3">
-                <ABTestPicker personal_abtests={personal_abtests} setSelectedABTest={setSelectedABTest} selected_abtest={selected_abtest}/>
+                <ABTestPicker personal_abtests={personal_abtests} setSelectedABTest={setSelectedABTest}
+                              selected_abtest={selected_abtest}/>
             </div>
-            {selected_abtest &&
+            {selected_abtest && <>
+
                 <div className="row text-center align-items-end">
                     <div>
                         <button className={"red-hover button-purple"}
@@ -112,21 +118,28 @@ function Statistics() {
                         </button>
                     </div>
                 </div>
+            </>
             }
             {selected_abtest && <>
                 <div className="row text-center align-content-center justify-content-center">
                     <h1>Used algorithms information</h1>
                     <Overview input_algorithms={input_algorithms}/>
                 </div>
+                <div className="row text-center align-content-center justify-content-center pt-5">
+                    <DateSlider dates={ABTest_data && ABTest_data.dates} setStartIndex={setStartIndex}
+                                setEndIndex={setEndIndex}>
+                    </DateSlider>
+                </div>
                 <div className="row text-center mt-5 align-content-center justify-content-center">
                     <h1>Charts</h1>
                 </div>
                 <div className="row text-center align-content-center justify-content-center">
                     <div className="col-12 col-lg-6 col-xl-6 col-xxl-6 pl-">
-                        <LineChart chart_id={1} title="Active Users" XFnY={activeUsersOverTime}/>
+                        <LineChart chart_id={1} title="Active Users" xMin={StartIndex} xMax={EndIndex}
+                                   XFnY={activeUsersOverTime}/>
                     </div>
                     <div className="col-12 col-lg-6 col-xl-6 col-xxl-6">
-                        <LineChart chart_id={2} title={"Purchases"} XFnY={purchases}/>
+                        <LineChart chart_id={2} title={"Purchases"} xMin={StartIndex} xMax={EndIndex} XFnY={purchases}/>
                     </div>
                     {/*<div className="col-12 col-lg-6 col-xl-6 col-xxl-6">*/}
                     {/*    <SearchUser selected_abtest={selected_abtest}/>*/}
@@ -134,7 +147,8 @@ function Statistics() {
                 </div>
                 <div className="row text-center align-content-center justify-content-center">
                     <div className="col-12 col-lg-6 col-xl-6 col-xxl-6 pl-">
-                        <LineChart chart_id={3} title="Click Through Rate" XFnY={clickThroughRate}/>
+                        <LineChart chart_id={3} title="Click Through Rate" xMin={StartIndex} xMax={EndIndex}
+                                   XFnY={clickThroughRate}/>
                     </div>
                     {/*<div className="col-12 col-lg-6 col-xl-6 col-xxl-6">*/}
                     {/*    <LineChart chart_id={2} title={"Purchases"} XFnY={purchases}/>*/}
