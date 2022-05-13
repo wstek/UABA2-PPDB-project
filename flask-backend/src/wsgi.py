f1abe6309bff86248aab31d494612e788131da1f
William-Steklein <st.william24@gmail.com>
2022-05-13 19:32:09 +0200

    changes to wsgi configuration

--- flask-backend/src/wsgi.py
+++ flask-backend/src/wsgi.py
@@ -7,8 +7,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.factories.app import create_app
 from src.factories.celery import configure_celery
 
+app = create_app()
+configure_celery(app)
+
 # RUN PRODUCTION SERVER
 if __name__ == "__main__":
-    app = create_app()
-    configure_celery(app)
     app.run()
