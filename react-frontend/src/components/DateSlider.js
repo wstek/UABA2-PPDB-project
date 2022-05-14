70d0306a1b41aa6194315d2474311a11f44c45f9
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2022-05-14 20:53:44 +0200

    improved DateSlider.js for small devices

--- react-frontend/src/components/DateSlider.js
+++ react-frontend/src/components/DateSlider.js
@@ -21,16 +21,16 @@ export default function DateSlider({dates, setStartIndex, setEndIndex}) {
     if (dates) {
         return (
             <>
-                <div className={"col-12 col-lg-3 col-xl-3 col-xxl-3 align-center text-lg-end my-auto"}>
-                    <input type="date" value={currentStartDate} id="start" className={"button-purple"}
-                           style={{'textAlign': 'center'}} name="trip-start" readOnly={true}/>
+                <div className={"col-12 align-items-md-center col-lg-3 col-xl-3 col-xxl-3 text-lg-end my-auto"}>
+                    <input type="date" value={currentStartDate} id="start" className={"dateField"}
+                           readOnly={true}/>
                 </div>
                 <div
-                    className={"col-12 col-lg-6 col-xl-6 col-xxl-6 align-content-center center text-center justify-content-center my-auto pt-sm-5 pb-sm-5"}>
+                    className={"col-12 col-lg-6 col-xl-6 col-xxl-6 align-content-center center pt-3 text-center justify-content-center my-auto pt-lg-0 pt-xl-0 pt-xxl-0 pb-lg-0 pb-xl-0 pb-xxl-0 pt-3 pb-3"}>
                     <MultiRangeSlider min={0} max={dates.length} onChange={sliderChanged}/>
                 </div>
                 <div className={"col-12 align-items-md-center col-lg-3 col-xl-3 col-xxl-3 text-lg-start my-auto"}>
-                    <input type="date" value={currentEndDate} id="end" name="trip-start " className={"button-purple"}
+                    <input type="date" value={currentEndDate} id="end" className={"dateField"}
                            readOnly={true}/>
                 </div>
             </>
