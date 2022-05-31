d2d91ba5ce6b501c818d9958ba7fb3b800011e8d
William-Steklein <st.william24@gmail.com>
2022-05-31 07:53:46 +0200

    updated reset_database.sh

--- sql/CreateTestAccount.sql
+++ sql/CreateTestAccount.sql
@@ -1,2 +1,9 @@
 insert into datascientist(username, datascientist_id, first_name, last_name, created_on, birthdate, password, email_address)
-values ('mosh', 1, 'mosh', 'mosh', '2022-05-30 13:34:40.407489', '2000-01-01', '$2b$12$KE6hSVmif9kZ9jnbU4t5ZOxcja/iOOUd8yjk1SBsP3K6M7b1jrOS.', 'mosh@mosh.mosh');
+values ('user', 1000, 'user', 'user', '2022-05-30 13:34:40.407489', '2000-01-01', '$2b$12$s3DgzH5Yvk1OpQyCYMT0fuI52uLOUt52p0iqoPpVrmSq8z5x345vq', 'user@user.user');
+insert into datascientist(username, datascientist_id, first_name, last_name, created_on, birthdate, password, email_address)
+values ('admin', 1001, 'admin', 'admin', '2022-05-30 13:34:40.407489', '2000-01-01', '$2b$12$I5wwMh7stAAnP3rdELrArOIdUeD14UIGp3n2ilnCuO4tS1JVkP4S6', 'admin@admin.admin');
+insert into datascientist(username, datascientist_id, first_name, last_name, created_on, birthdate, password, email_address)
+values ('mosh', 1003, 'mosh', 'mosh', '2022-05-30 13:34:40.407489', '2000-01-01', '$2b$12$p.iSFh0v674YuTWF1tncRuVTTPAGKSBHwnyavYvNz5AMN6WXwVbfi', 'mosh@mosh.mosh');
+
+insert into admin(username) values ('admin');
+insert into admin(username) values ('mosh');
