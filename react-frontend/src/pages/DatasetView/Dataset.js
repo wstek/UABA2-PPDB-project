279d77e2f034478f1b0cab9f2f10d3be8ca7d1f6
Sam.Roggeman@Gmail.com <Sam.Roggeman@gmail.com>
2022-05-31 11:33:24 +0200

    dataset change on selecting a new one to view

--- react-frontend/src/pages/DatasetView/Dataset.js
+++ react-frontend/src/pages/DatasetView/Dataset.js
@@ -17,7 +17,7 @@ export function DatasetStatistics() {
             setDatasetInformation(data)
         })
     }
-    useEffect(fetchDatasetInformation, [],)
+    useEffect(fetchDatasetInformation, [dataset_name],)
 
     if (!dataset_information) return <PurpleSpinner/>
     // console.log(dataset_information)
@@ -79,7 +79,8 @@ export default function DatasetPage() {
                            onChange={(selected_dataset) => history.push('/dataset/' + selected_dataset)}
                 // setSelectedInput={setSelectedDataset}
                            selected_input={dataset_name}
-                           header={"Select Dataset"}
+                           header={"" +
+                               "Select Dataset"}
             />
         </div>
         {/*{selected_dataset && <Redirect to={'dataset/'+selected_dataset}></Redirect> }*/}
